9fccfa47e411e8dc124fdb3eede35d7fec8bda53
Salimov Ruslan <salimow.r2015@icloud.com>
2021-05-05 09:30:31 +0500

    Add InsertionSorter

---
+++ Sorting/InsertionSorter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Sorting
+{
+    // Worst-case performance: О(n^2) 
+    // Best-case performance: O(n)
+    // Average performance: O(n^2)
+    public static class InsertionSorter
+    {
+        public static void InsertionSort<T>(this IList<T> collection, IComparer<T> comparer = null)
+        {
+            comparer ??= Comparer<T>.Default;
+            collection.SelectionSortAscending(comparer);
+        }
+        
+        public static void InsertionSortAscending<T>(this IList<T> collection, IComparer<T> comparer)
+        {
+            for (int i = 1; i < collection.Count; i++)
+            {
+                var j = i;
+                while (j > 0 && comparer.Compare(collection[j], collection[j - 1]) < 0)
+                {
+                    collection.Swap(j, j - 1);
+                    j--;
+                }
+            }
+        }
+        
+        public static void InsertionSortDescending<T>(this IList<T> collection, IComparer<T> comparer)
+        {
+            for (int i = 1; i < collection.Count; i++)
+            {
+                var j = i;
+                while (j > 0 && comparer.Compare(collection[j], collection[j - 1]) > 0)
+                {
+                    collection.Swap(j, j - 1);
+                    j--;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Sorting.Tests/InsertionSorter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Sorting.Tests
+{
+    public class InsertionSorter
+    {
+        [Fact]
+        public void InsertionSort()
+        {
+            var actual = new List<int>();
+            actual.AddRange(new [] {5, 3, 1, 4, 2});
+            var expected = new List<int>();
+            expected.AddRange(new [] { 1, 2, 3, 4, 5 });
+            
+            actual.InsertionSort();
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Fact]
+        public void InsertionSortAscending()
+        {
+            var actual = new List<int>();
+            actual.AddRange(new [] {5, 3, 1, 4, 2});
+            var expected = new List<int>();
+            expected.AddRange(new [] { 1, 2, 3, 4, 5 });
+            
+            actual.InsertionSortAscending(Comparer<int>.Default);
+            
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void InsertionSortDescending()
+        {
+            var actual = new List<int>();
+            actual.AddRange(new [] {2, 3, 1, 4, 5});
+            var expected = new List<int>();
+            expected.AddRange(new [] { 5, 4, 3, 2, 1 });
+            
+            actual.InsertionSortDescending(Comparer<int>.Default);
+            
+            Assert.Equal(expected, actual);
+        }
+    }
+}
