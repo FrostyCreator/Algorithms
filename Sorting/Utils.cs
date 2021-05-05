ffd5fa3e9f9ee41f27059b851c2dfcf4dcb95a2d
Salimov Ruslan <salimow.r2015@icloud.com>
2021-05-05 09:30:09 +0500

    Add Utils

---
+++ Sorting/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sorting
+{
+    public static class Utils
+    {
+        public static void Swap<T>(this IList<T> collection, int a, int b)
+        {
+            T temp = collection[a];
+            collection[a] = collection[b];
+            collection[b] = temp;
+        }
+    }
+}
